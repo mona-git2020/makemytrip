55e8f172a93b83b58a9c75c930aaba8e6d8d45b7
mona-git2020 <monalisha.prajapati@einfochips.com>
2020-07-02 20:08:38 +0530

    completed search for hotels

--- TestCases/Variables/Variables.py
+++ TestCases/Variables/Variables.py
@@ -2,13 +2,16 @@
 
 browser = 'FireFox'
 plateform = 'WIN10'
-url = 'https://www.makemytrip.com/'
+url = 'https://www.makemytrip.com/hotels/hotel-listing/?checkin=07052020&city=CTDIU&checkout=07072020&roomStayQualifier=2e2e11e5e&locusId=CTDIU&country=IN&locusType=city&searchText=Diu,%20Daman%20and%20Diu,%20India&visitorId=f399ec2b-c28a-47c5-ba31-f602cdd3f250'
 
 
 destination = 'Diu'
-checkin = 'Thu Jul 02 2020'
-checkout = 'Thu Jul 04 2020'
+checkin = 'Thu Jul 05 2020'
+checkout = 'Thu Jul 07 2020'
 adultsnum = 2
 childsnum = 2
 age0 = 10
 age1 = 5
+
+
+popularFilters = ["MMT Assured", "MySafety", "Staycation", "Freecancellation"]
