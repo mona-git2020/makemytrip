2afcafbc3d3ce19069366805c751bfc4f9701f97
mona-git2020 <monalisha.prajapati@einfochips.com>
2020-07-06 20:09:23 +0530

    find all filter locators

--- TestCases/Variables/Variables.py
+++ TestCases/Variables/Variables.py
@@ -1,17 +1,20 @@
 #*** Variables ***
 
 browser = 'FireFox'
-plateform = 'WIN10'
-url = 'https://www.makemytrip.com/hotels/hotel-listing/?checkin=07052020&city=CTDIU&checkout=07072020&roomStayQualifier=2e2e11e5e&locusId=CTDIU&country=IN&locusType=city&searchText=Diu,%20Daman%20and%20Diu,%20India&visitorId=f399ec2b-c28a-47c5-ba31-f602cdd3f250'
+url = 'https://www.makemytrip.com/hotels/hotel-listing/?checkin=07062020&checkout=07082020&city=CTDIU&country=IN&locusId=CTDIU&locusType=city&roomStayQualifier=2e2e11e5e&searchText=Diu%2C%20Daman%20and%20Diu%2C%20India&visitorId=f399ec2b-c28a-47c5-ba31-f602cdd3f250'
 
 
 destination = 'Diu'
-checkin = 'Thu Jul 05 2020'
-checkout = 'Thu Jul 07 2020'
+checkin = 'Thu Jul 10 2020'
+checkout = 'Thu Jul 12 2020'
 adultsnum = 2
 childsnum = 2
 age0 = 10
 age1 = 5
 
 
-popularFilters = ["MMT Assured", "MySafety", "Staycation", "Freecancellation"]
+popularFiltersList = ["MMT Assured", "MySafety - Safe and Hygienic Stays", "Pay @ Hotel Available", "EMI Deals Available", "LOW_COST_EMI", "Free Breakfast", "Free Cancellation, Zero Payment Now", "Free Cancellation"]
+
+#popularFiltersList = ["Free Breakfast", "Free Cancellation, Zero Payment Now", "Free Cancellation"]
+
+#popularFilterList[0]
