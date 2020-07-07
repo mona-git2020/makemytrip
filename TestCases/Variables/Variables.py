7288514fa17c620011badebfac26b13a0d2bd995
mona-git2020 <monalisha.prajapati@einfochips.com>
2020-07-07 20:41:44 +0530

    Test case 7 completed

--- TestCases/Variables/Variables.py
+++ TestCases/Variables/Variables.py
@@ -1,7 +1,7 @@
 #*** Variables ***
 
 browser = 'FireFox'
-url = 'https://www.makemytrip.com/hotels/hotel-listing/?checkin=07062020&checkout=07082020&city=CTDIU&country=IN&locusId=CTDIU&locusType=city&roomStayQualifier=2e2e11e5e&searchText=Diu%2C%20Daman%20and%20Diu%2C%20India&visitorId=f399ec2b-c28a-47c5-ba31-f602cdd3f250'
+url = 'https://www.makemytrip.com/hotels/hotel-listing/?checkin=07102020&checkout=07122020&locusId=CTDIU&locusType=city&city=CTDIU&country=IN&searchText=Diu%2C%20Daman%20and%20Diu%2C%20India&roomStayQualifier=2e2e11e5e&_uCurrency=INR&reference=hotel&type=city'
 
 
 destination = 'Diu'
@@ -11,10 +11,16 @@ adultsnum = 2
 childsnum = 2
 age0 = 10
 age1 = 5
-
+allFilters = ["Popular", "Locality", "Star Category", "User Rating", "Property Types", "Ficility"]
 
 popularFiltersList = ["MMT Assured", "MySafety - Safe and Hygienic Stays", "Pay @ Hotel Available", "EMI Deals Available", "LOW_COST_EMI", "Free Breakfast", "Free Cancellation, Zero Payment Now", "Free Cancellation"]
 
-#popularFiltersList = ["Free Breakfast", "Free Cancellation, Zero Payment Now", "Free Cancellation"]
+localityFilterList = ["Diu Airport", "Diu Fort", "Nagoa Beach", "Bandar Chowk"]
 
-#popularFilterList[0]
+starCategoryList = ["Unrated", "4 Star", "3 Star", "2 Star", "1 Star"]
+
+userRatingList = ["4.5 & above(Excellent)", "4 & above(Very Good)", "3 & above(Good)"]
+
+propertyTypeList = ["Hotel", "RESORT", "GUEST HOUSE", "HOMESTAY", "APART-HOTEL", "Camp", "Farm House"]
+
+facilityFilterList = ["Caretaker", "Cafe", "Kitchenette", "Elevator/Lift", "Spa", "Restaurant", "Bar", "Indoor Games", "Parking", "Balcony/Terrace", "Living Room", "Bonfire", "Facilities for Guests with Disabilities", "Swimming Pool", "Wi-Fi", "Barbeque", "Fireplace", "Room Service"]
