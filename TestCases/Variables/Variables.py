b48deed78e1e8599789003bd886bb9d6b5329d96
mona-git2020 <monalisha.prajapati@einfochips.com>
2020-07-13 20:49:25 +0530

    Test Case 5(Filter hotels according to Star Category) complted

--- TestCases/Variables/Variables.py
+++ TestCases/Variables/Variables.py
@@ -1,4 +1,4 @@
-#*** Variables ***
+
 
 browser = 'FireFox'
 url = 'https://www.makemytrip.com/hotels/hotel-listing/?checkin=07142020&city=CTDIU&checkout=07162020&roomStayQualifier=2e2e10e6e&locusId=CTDIU&country=IN&locusType=city&searchText=Diu,%20Daman%20and%20Diu,%20India&visitorId=f399ec2b-c28a-47c5-ba31-f602cdd3f250'
@@ -17,7 +17,7 @@ popularFiltersList = ["MMT Assured", "MySafety - Safe and Hygienic Stays", "Pay 
 
 localityFilterList = {"Near Diu Airport" : "Diu Airport", "Near Diu Fort" : "Diu Fort", "Near Bandar Chowk" : "Bandar Chowk"}
 
-starCategoryList = ["Unrated", "4 Star", "3 Star", "2 Star", "1 Star"]
+starCategoryFilterList = {"Unrated" : "Unrated", "4 Star" : "4", "3 Star" : "3", "2 Star" : "2", "1 Star" : "1"}
 
 userRatingList = ["4.5", "4.0", "3.0"]
 
