524d28237d7bbe16257b71ae1df0623a5a84cb54
mona-git2020 <monalisha.prajapati@einfochips.com>
2020-07-21 21:52:55 +0530

    Made some changes according to review comments

--- TestCases/Variables/Variables.py
+++ TestCases/Variables/Variables.py
@@ -1,12 +1,12 @@
 
 
 
-url = 'https://www.makemytrip.com/hotels/hotel-listing/?checkin=07232020&city=CTDIU&checkout=07252020&roomStayQualifier=2e2e10e6e&locusId=CTDIU&country=IN&locusType=city&searchText=Diu,%20Daman%20and%20Diu,%20India&visitorId=f399ec2b-c28a-47c5-ba31-f602cdd3f250'
-
+#url = 'https://www.makemytrip.com/hotels/hotel-listing/?checkin=07232020&city=CTDIU&checkout=07252020&roomStayQualifier=2e2e10e6e&locusId=CTDIU&country=IN&locusType=city&searchText=Diu,%20Daman%20and%20Diu,%20India&visitorId=f399ec2b-c28a-47c5-ba31-f602cdd3f250'
+url = 'https://www.makemytrip.com/'
 
 destination = 'Diu'
-checkin = 'Thu Jul 10 2020'
-checkout = 'Thu Jul 12 2020'
+checkin = 'Fri Jul 24 2020'
+checkout = 'Sun Jul 26 2020'
 adultsnum = 2
 childsnum = 2
 age0 = 10
