3fb9b16aa93270f1b0ad525129724fbb75c14e25
mona-git2020 <monalisha.prajapati@einfochips.com>
2020-08-07 20:43:05 +0530

    I have made changes in the Test Cases

--- TestCases/Variables/Variables.py
+++ TestCases/Variables/Variables.py
@@ -1,19 +1,17 @@
 
 
 
-urlOfListedHotels = 'https://www.makemytrip.com/hotels/hotel-listing/?checkin=08062020&city=CTDIU&checkout=08072020&roomStayQualifier=2e2e10e6e&locusId=CTDIU&country=IN&locusType=city&searchText=Diu,%20Daman%20and%20Diu,%20India&visitorId=f399ec2b-c28a-47c5-ba31-f602cdd3f250'
+urlOfListedHotels = 'https://www.makemytrip.com/hotels/hotel-listing/?checkin=08092020&city=CTDIU&checkout=08102020&roomStayQualifier=2e2e10e6e&locusId=CTDIU&country=IN&locusType=city&searchText=Diu,%20Daman%20and%20Diu,%20India&visitorId=f399ec2b-c28a-47c5-ba31-f602cdd3f250'
 urlOfMakeMyTrip = 'https://www.makemytrip.com/'
 
 destination = 'Diu'
-checkin = 'Fri Jul 24 2020'
-checkout = 'Sun Jul 26 2020'
-adultsnum = 2
-childsnum = 2
-age0 = 10
-age1 = 5
+adultsNum = 2
+childsNum = 2
+ageChild1 = 10
+ageChild2 = 5
 allFilters = ["Popular", "Locality", "Star Category", "User Rating", "Property Types", "Ficility"]
 
-popularFiltersList = {"MMT Assured" : "MMT Assured", "MySafety - Safe and Hygienic Stays" : "MySafety - Safe and Hygienic Stays", "Pay @ Hotel Available" : "Pay@Hotel", "EMI Deals Available" : "EMI", "LOW_COST_EMI" : "EMI", "Free Breakfast" : "Free Breakfast", "Free Cancellation, Zero Payment Now" : "Free Cancellation, Zero Payment Now", "Free Cancellation" : "Free Cancellation"}
+popularFiltersList = {"MMT Assured" : "MMT Assured", "MySafety - Safe and Hygienic Stays" : "MySafety - Safe and Hygienic Stays", "Pay @ Hotel Available" : "Pay@Hotel", "EMI Deals Available" : "EMI", "Low Cost EMI" : "EMI", "No Cost EMI" : "EMI", "Free Breakfast" : "Free Breakfast", "Free Cancellation, Zero Payment Now" : "Free Cancellation, Zero Payment Now", "Free Cancellation" : "Free Cancellation"}
 
 localityFilterList = {"Diu Airport (DIU)" : "Diu Airport", "Diu Fort" : "Diu Fort", "Bandar Chowk" : "Bandar Chowk", "Chowk Market" : "Chowk"}
 
