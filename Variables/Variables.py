42eb1866325b590804f8fa4f02e2c983c0049830
mona-git2020 <monalisha.prajapati@einfochips.com>
2020-09-01 22:38:30 +0530

    Remaining two files

---
+++ Variables/Variables.py
@@ -0,0 +1,27 @@
+
+
+
+urlOfListedHotels = 'https://www.makemytrip.com/hotels/hotel-listing/?checkin=09112020&city=CTDIU&checkout=09122020&roomStayQualifier=2e2e10e6e&locusId=CTDIU&country=IN&locusType=city&searchText=Diu,%20Daman%20and%20Diu,%20India&visitorId=f399ec2b-c28a-47c5-ba31-f602cdd3f250'
+urlOfMakeMyTrip = 'https://www.makemytrip.com/'
+
+destination = 'Diu'
+adultsNum = 2
+childsNum = 2
+ageChild1 = 10
+ageChild2 = 5
+nearestLocations = ["Una", "Ghoghola"]
+
+allFilters = ["Popular", "Locality", "Star Category", "User Rating", "Property Types", "Ficility"]
+
+popularFiltersList = {"MMT Assured" : "MMT Assured", "MySafety - Safe and Hygienic Stays" : "MySafety - Safe and Hygienic Stays", "Pay @ Hotel Available" : "Pay@Hotel", "EMI Deals Available" : "EMI", "Low Cost EMI" : "EMI", "No Cost EMI" : "EMI", "Free Breakfast" : "Free Breakfast", "Free Cancellation, Zero Payment Now" : "Free Cancellation, Zero Payment Now", "Free Cancellation" : "Free Cancellation"}
+
+localityFilterList = {"Diu Airport (DIU)" : "Diu Airport", "Diu Fort" : "Diu Fort", "Bandar Chowk" : "Bandar Chowk", "Chowk Market" : "Chowk"}
+
+starCategoryFilterList = {"Unrated" : "Unrated", "4 Star" : "4", "3 Star" : "3", "2 Star" : "2", "1 Star" : "1"}
+
+userRatingList = ["4.5", "4.0", "3.0"]
+
+
+propertyTypeList = ["Hotels", "Resort", "Guest House", "Apartment", "Camp", "Homestay"]
+
+facilityFilterList = {"Restaurant" : "Restaurant", "Swimming Pool" : "Swimming Pool", "Cafe" : "Cafe", "Room Service" : "Room Service", "Indoor Games" : "Indoor Games", "Outdoor Sports" : "Outdoor Sports", "Facilities for Guests with Disabilities" : "Facilities for Guests with Disabilities", "Railway Station Transfers" : "Railway Station Transfers", "Airport Transfers" : "Airport Transfer", "Bus Station Transfers" : "Bus Station Transfer", "Bar" : "Bar", "Balcony/Terrace" : "Balcony Terrace", "Fireplace" : "Fireplace", "Wi-Fi" : "Wi-Fi", "Caretaker" : "Caretaker", "Kitchenette" : "Kitchenette", "Bonfire" : "Bonfire", "Elevator/Lift" : "Lift", "Spa" : "Spa", "Parking" : "Parking", "Living Room" : "Living Room", "Barbeque" : "Barbeque"}
